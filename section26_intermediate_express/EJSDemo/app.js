9a65b2488da2e388a00a81decce5fb84c869230d
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2019-09-20 08:28:59 -0400

    notes up to lect273. must finish lect274(7:00)

--- section26_intermediate_express/EJSDemo/app.js
+++ section26_intermediate_express/EJSDemo/app.js
@@ -1,6 +1,9 @@
 var express = require("express")
 var app = express();
 
+app.use(express.static("public"));
+
+
 app.get("/", function(req, res){
     res.render("home.ejs")
 });
@@ -9,6 +12,17 @@ app.get("/fallinlovewith/:thing", function(req, res){
     var thing = req.params.thing;
     res.render("love.ejs", {thingVar : thing});
 });
+
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "Post 1", author : "sussy"},
+        {title: "My adorable Gafdsf", author : "lucas"},
+        {title: "Can you belive this!???", author : "sasfasdg"}
+    ];
+
+    res.render("posts.ejs", {posts: posts});
+});
+
 
 app.listen(3001 ,function(){
     console.log("Server is listening!!!");
